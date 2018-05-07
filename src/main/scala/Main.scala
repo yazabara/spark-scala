348e217c935ed4c2db097ae547f166baea3a7baa
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-05-07 12:14:49 +0300

    scala code examples added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,43 @@
 
 object Main extends App {
+
+  /**
+    * executable app code
+    */
   val ages = Seq(42, 75, 29, 64)
   println(s"The oldest person is ${ages.max}")
 
+  val list = List(
+    Set(1,2,3,4,5),
+    Set(1,2,3,6,7)
+  )
+
+  //with duplicates (list)
+  println(s"Flatten set list is ${list.flatten.sorted}")
+
+  val set = Set(
+    Set(1,2,3,4,5),
+    Set(1,2,3,6,7)
+  )
+
+  //without duplicates (set)
+  println(s"Flatten set with sets is ${set.flatten}")
+
   def cube(x: Int) = {
     x * x * x
+  }
+
+  /**
+    * Higher-order functions
+    *
+    * @param func function to apply with a & b arguments
+    * @param a    first argument
+    * @param b    second argument
+    * @return
+    */
+  def sum(func: Int => Int, a: Int, b: Int): Int = func(a) + func(b)
+
+  def cubeSum(a: Int, b: Int) = {
+    sum(cube, a, b)
   }
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -3,4 +3,12 @@ class MainTest extends org.scalatest.FunSuite {
   test("Simple scala test") {
     assert(Main.cube(3) === 27)
   }
+
+  test("Higher-order functions test 1") {
+    assert(Main.sum(x => x + x, 3, 3) === 6 + 6)
+  }
+
+  test("Higher-order functions test 2") {
+    assert(Main.cubeSum(3, 3) === 27 + 27)
+  }
 }
